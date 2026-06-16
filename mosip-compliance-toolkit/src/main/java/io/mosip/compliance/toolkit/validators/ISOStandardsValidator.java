39ec5562dca2d3ca127fbe1851ab58c31d8cd0ad
Chetan Kumar Hirematha <chetankumar.h.239@gmail.com>
2026-06-16 14:50:38 +0530

    [MOSIP-42453]: Reject Face Image if Not in 24-bit RGB Format
    Signed-off-by: Chetan Kumar Hirematha <chetankumar.h.239@gmail.com>

--- mosip-compliance-toolkit/src/main/java/io/mosip/compliance/toolkit/validators/ISOStandardsValidator.java
+++ mosip-compliance-toolkit/src/main/java/io/mosip/compliance/toolkit/validators/ISOStandardsValidator.java
@@ -1867,10 +1867,17 @@ public class ISOStandardsValidator extends SBIValidator {
 				isValidWarnings = true;
 			}
 			if (!FaceISOStandardsValidator.getInstance().isValidImageColorSpace(purpose, Modality.Face, decoderRequestDto)) {
-				warningMessage.append(
-						"<BR>Invalid Image Color Space allowed values Up to RGB[24 bit]");
-				warningMsgCode.append("ISO_WARNING_012");
-				isValidWarnings = true;
+				message.append(
+						"<BR>Invalid Image Color Space for Face Modality, expected RGB[24 bit], but received colorSpace["
+								+ decoderRequestDto.getImageColorSpace() + "], depth[" + decoderRequestDto.getDepth()
+								+ "]");
+				code.append(AppConstants.COMMA_SEPARATOR);
+				code.append("ISO_VALIDATOR_118");
+				code.append(AppConstants.ARGUMENTS_DELIMITER);
+				code.append(decoderRequestDto.getImageColorSpace());
+				code.append(AppConstants.ARGUMENTS_SEPARATOR);
+				code.append(decoderRequestDto.getDepth());
+				isValid = false;
 			}
 			if (!FaceISOStandardsValidator.getInstance().isValidImageDPI(purpose, Modality.Face, decoderRequestDto)) {
 				warningMessage.append(
